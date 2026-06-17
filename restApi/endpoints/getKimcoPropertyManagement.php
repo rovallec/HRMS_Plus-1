e9bc301e8190dc9952cc8205156a34d6edee0597
rovallec <raul.ovalle@nearsol.gt>
2026-06-17 13:31:14 -0600

    commitToProd

--- restApi/endpoints/getKimcoPropertyManagement.php
+++ restApi/endpoints/getKimcoPropertyManagement.php
@@ -1,7 +1,7 @@
 <?php
 
 header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: http://localhost:4200");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, X-SIGNATURE");
 header("Access-Control-Max-Age: 86400");
